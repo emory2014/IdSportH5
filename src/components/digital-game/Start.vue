021f46cd7150b781f8d2a3b0f6e0a3c301f9315b
田凡 <>
2018-08-14 20:55:25 +0800

    update

--- src/components/digital-game/Start.vue
+++ src/components/digital-game/Start.vue
@@ -245,7 +245,7 @@ let Base64 = require('js-base64').Base64
                             this.start = true
                             clearInterval(timeout)
                 }
-                },1000)
+                },1500)
             },
             proccessActive(time){
                  clearTimeout(timeout1)
