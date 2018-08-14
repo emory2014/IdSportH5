fa794636e7de9d8c95c5976834f80299aae3a711
田凡 <>
2018-08-14 22:46:20 +0800

    merge update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,19 +1,10 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-<<<<<<< HEAD
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "postcss-import": {},
     "autoprefixer": {}
   }
 }
-=======
-    "plugins": {
-      // to edit target browsers: use "browserslist" field in package.json
-      "postcss-import": {},
-      "autoprefixer": {}
-    }
-  }
-  
->>>>>>> 0a4fae9782642c2dc4fbdb6b4397d37b88d83cde
+
