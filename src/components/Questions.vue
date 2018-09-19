71a153f52b54defeb6247f665a44893cd90c31b1
田凡 <>
2018-09-19 19:51:02 +0800

    update

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -2,35 +2,77 @@
   <div class="question-container">
           <BHeader title="Pertanyaan Umum " backToApp={true} />
           <div class="ques-btn-group">
-                <span class="tag active">Member Baru</span>
-                <span class="tag">RPC</span>
-                <span class="tag">Mengundang</span>
+                <a href="#baru" @click="() => { active = 1 }"><span class="tag " :class="[active == 1 ? 'active':'']">Member Baru</span></a>
+                <a href="#rpc" @click="() => { active = 2 }"><span class="tag" :class="[active == 2 ? 'active':'']">RPC</span></a>
+                <!-- <span class="tag">Mengundang</span>
                 <span class="tag">Penarikan</span>
-                <span class="tag">Akun</span>
+                <span class="tag">Akun</span> -->
           </div>
-          <div class="ques-title">
+          <div class="ques-title" id="baru">
               Member Baru
           </div>
-          <p class="sub-title open">Kenapa NewsCat bisa kasih bonus? <i class="icon-arrow"></i></p>
-          <div class="ques-anwser">
-               NewsCat mendukung untuk menyediakan 
-                pengalaman membaca bacaan yang berkualitas.
-                Di dalam pengalaman membaca tersebut, kami 
-                memberikan apresiasi dalam bentuk RPC, sebagai 
-                pengganti uang. 
-                Jadi selain kalian bisa menambah pengetahuan 
-                menjadi lebih luas, kalian juga bisa mendapatkan 
-                penghasilan.
+          <p class="sub-title " :class="[ open == 1 ? 'open':'']" @click="fold(1)">Kenapa NewsCat bisa kasih bonus? <i class="icon-arrow"></i></p>
+          <div class="ques-anwser" :class="[ open == 1 ? 'open':'']" @click="fold(1)">
+               NewsCat mendukung peyediaan pengalaman membaca bacaan yang berkualitas.
+
+                Di dalam pengalaman membaca tersebut, kami memberikan apresiasi dalam bentuk RPC, sebagai pengganti uang.
+
+                Selain kalian bisa menambah pengetahuan menjadi lebih luas, kalian juga bisa mendapatkan penghasilan.
           </div>
-          <p class="sub-title">Cara menggunakan NewsCat <i class="icon-link"></i></p>
-          <p class="sub-title">Berita tidak muncul <i class="icon-arrow"></i></p>
-          <div class="ques-title">
+          <!-- <p class="sub-title">Cara menggunakan NewsCat <i class="icon-link"></i></p> -->
+          <p class="sub-title" :class="[ open == 2 ? 'open':'']" @click="fold(2)">Berita tidak muncul <i class="icon-arrow"></i></p>
+          <div class="ques-anwser" :class="[ open == 2 ? 'open':'']" @click="fold(2)">
+              Apakah kamu tidak bisa memuat semua berita atau hanya sebagian saja?
+
+                Silahkan untuk meng-screenshoot error atau masalah dengan judul berita
+
+                dan kirimkan ke kami di Kendala Customer. Kami akan segera memeriksanya.
+          </div>
+          <div class="ques-title" id="rpc">
               RPC
           </div>
-          <p class="sub-title">Apa itu RPC?<i class="icon-arrow"></i></p>
-          <p class="sub-title">Bonus Absen/Bonus Buka Peti Emas<i class="icon-arrow"></i></p>
-          <p class="sub-title">Baca Berita tidak dapat RPC terus bagaimana?<i class="icon-arrow"></i></p>
-          <div class="ques-title">
+          <p class="sub-title" :class="[ open == 3 ? 'open':'']" @click="fold(3)">Apa itu RPC?<i class="icon-arrow"></i></p>
+          <div class="ques-anwser" :class="[ open == 3 ? 'open':'']" @click="fold(3)">
+            RPC adalah sebuah unit mata uang didalam NewsCat. Semua RPC yang dihasilkan bisa anda tukarkan
+
+            dengan Pencairan Cepat atau Pencairan Bersyarat, bisa ditemukan di "Saya" -> "Penghasilan Saya"
+          </div>
+          <p class="sub-title" :class="[ open == 4 ? 'open':'']" @click="fold(4)">Bonus Absen/Bonus Buka Peti Emas<i class="icon-arrow"></i></p>
+          <div class="ques-anwser" :class="[ open == 4 ? 'open':'']" @click="fold(4)">
+            Bonus Absen : Buka NewsCat selama 7 hari secara berurutan pada
+
+            "Tugas" bisa dapat bonus RPC, setiap hari bisa diambil! Bonus dibagi
+
+            bertahap 7 hari dengan jumlah RPC 30, 35, 40, 135, 30, 35, 235. Jika
+
+            ada 1 hari absen yang bolong, maka bonus akan diulang pada hari pertama.
+
+            Bonus Buka Kotak Emas : Setiap 4 jam sekali buka bisa dapat bonus!
+          </div>
+          <p class="sub-title" :class="[ open == 5 ? 'open':'']" @click="fold(5)">Baca Berita tidak dapat RPC terus bagaimana?<i class="icon-arrow"></i></p>
+          <div class="ques-anwser" :class="[ open == 5 ? 'open':'']" @click="fold(5)">
+            Situasi 1: Sebagian besar berita perlu dimuat selama beberapa saat
+
+            untuk mendapatkan koin emas, jadi harap bersabar menunggu.<br>
+
+
+            Situasi 2 : Mungkin sebagian berita sudah kamu baca, bonus juga sudah
+
+            didapat, setiap berita hanya bisa dapat bonus 1 kali.<br>
+
+
+            Situasi 3 : Bonus dari berita sudah masuk, tapi karena jaringan bermasalah,
+
+            pemberitahuan bonus tidak muncul.<br>
+
+
+            Situasi 4 : Silahkan periksa kembali versi aplikasi NewsCat pastikan versi
+
+            NewsCat kamu adalah yang paling baru, jika belum silahkan perbaharui
+
+            ke versi paling baru.<br>
+          </div>
+          <!-- <div class="ques-title">
               Mengundang
           </div>
           <p class="sub-title">Bagaimana cara mengundang teman?<i class="icon-arrow"></i></p>
@@ -43,7 +85,7 @@
           </div>
           <p class="sub-title">Tidak bisa daftar?<i class="icon-arrow"></i></p>
           <p class="sub-title two">Peraturan jika kedapatan curang dan<br> hukumannya<i class="icon-arrow"></i></p>
-          <p class="sub-title">Kenapa  akun saya diblokir?<i class="icon-arrow"></i></p>
+          <p class="sub-title">Kenapa  akun saya diblokir?<i class="icon-arrow"></i></p> -->
 
   </div>
 </template>
@@ -56,8 +98,19 @@ export default {
   },
   data() {
     return {
-
+        active: 1,
+        open: 0
     }
+  },
+  methods: {
+      fold(index){
+          if(this.open == index) {
+              this.open = 0
+          }else{
+              this.open = index
+
+          }
+      }
   }
 }
 </script>
@@ -71,8 +124,9 @@ body{
 }
 
 .ques-btn-group {
-    padding: 20px 20px 0 20px;
+    padding: 12px 20px 0 20px;
     box-sizing: border-box;
+    margin-top: 20px;
 
 }
 
@@ -125,10 +179,17 @@ body{
 }
 
 .ques-anwser {
-    padding: 15px;
+    padding: 0;
     background: #f5f5f5;
     font-size: 14px;
     color: #666;
+    height: 0;
+    overflow: hidden;
+}
+
+.open.ques-anwser {
+    padding: 15px;
+    height: 100%;
 }
 
 .icon-arrow {
