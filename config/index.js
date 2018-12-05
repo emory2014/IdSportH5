71bd847b752f157969a30bd9c70fe1f865aa6186
饭饭紫 <1826884431@qq.com>
2018-12-05 19:38:21 +0800

    update

--- config/index.js
+++ config/index.js
@@ -63,8 +63,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    // assetsPublicPath: 'http://front.danaunta.com/',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://front.danaunta.com/',
+    // assetsPublicPath: '/',
     /**
      * Source Maps
      */
