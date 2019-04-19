0b7d953529a7b64edaa1e6fa6ab569272ed86570
孟磊 <menglei@mengleideMac-mini.local>
2019-04-19 16:35:15 +0800

    task

--- src/router/index.js
+++ src/router/index.js
@@ -44,8 +44,8 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
-  mode: 'hash',
+  mode: 'history',
+  // mode: 'hash',
   base: '/web/',
   routes: [
     // {
