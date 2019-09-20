9ca6a6f63fce12d5e18e527e99ff59ce083ac91c
sharon <shurong.liuy@gmail.com>
2019-09-20 18:03:54 +0800

    update

--- src/components/recharge/Process.vue
+++ src/components/recharge/Process.vue
@@ -22,7 +22,7 @@
             <li> Jika kasir tidak mengetahui mengenai pembayaran Nama Merchant, informasikan ke kasir untuk membuka terminal e-transaction, pilih "no. 2 menu Pembayaran atau cari pada menu search" c </li>
             <li> Minta kasir untuk menekan “Shift” dan “?” lalu ketik Nama Merchant “WAYANG PAY”, setelah itu klik OK </li>
             <li>Minta kasir untuk pilih "no.1 WAYANG PAY - Pembayaran WAYANG PAY" </li>
-            <li>Kasir akan menanyakan Kode Pembayaran. Berikan Kode Pembayaran anda <span class="yellow">{{data}}</span>. Kasir akan melakukan konfirmasi data konsumen berupa Nama Merchant “BMY”, Nama Konsumen, dan Nominal. Lakukan pembayaran ke
+            <li>Kasir akan menanyakan Kode Pembayaran. Berikan Kode Pembayaran anda <span class="yellow">{{data}}</span>. Kasir akan melakukan konfirmasi data konsumen berupa Nama Merchant “WAYANG PAY”, Nama Konsumen, dan Nominal. Lakukan pembayaran ke
               kasir sejumlah nominal yang disebutkan </li>
             <li>Terima struk sebagai bukti pembayaran sudah sukses dilakukan. Notifikasi pembayaran akan langsung diterima oleh Merchant</li>
             <li>Selesai.</li>
