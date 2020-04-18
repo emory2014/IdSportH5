be9c1262693b560a54d3f4534ec6c63b7d2243bc
饭饭紫 <1826884431@qq.com>
2020-04-18 15:46:35 +0800

    update

--- config/index.js
+++ config/index.js
@@ -62,8 +62,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://front.danaunta.com/',
-    // assetsPublicPath: '/',
+    // assetsPublicPath: 'http://front.danaunta.com/',
+    assetsPublicPath: '/',
     /**
      * Source Maps
      */
