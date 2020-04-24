5c1e90386852ca4aa799cd3b32eec9876cdba8ce
饭饭紫 <1826884431@qq.com>
2020-04-24 18:55:38 +0800

    test

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -20,7 +20,9 @@ export default {
         if (process.env.NODE_ENV === 'production') {
           var content = window.AndroidWebView.getAppToken();
           var token = Base64.decode(content)
+          window.AndroidWebView.showContent(content)
           return token
+
         }else {
           return "9e917f900eb290f67a3b5c6d5db8237b"
         }
