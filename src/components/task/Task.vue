8ab9eba9d5f3c337c82b95c83d929970e5a099c6
饭饭紫 <1826884431@qq.com>
2020-04-26 17:39:42 +0800

    updte

--- src/components/task/Task.vue
+++ src/components/task/Task.vue
@@ -1,11 +1,7 @@
 <template>
   <div style="padding-top:110px" class="taskBox">
-    <!-- <header class="beheader">Tugas</header> -->
-    <header class="fixHeader" style="paddingTop: '18px'">
-    　<i class="nc-icon-prev" @click="goBack()" style="top: 38px"></i>
-    <div>Tugas</div>
+    <header class="beheader">Tugas</header>
 
-  </header>
     <!-- 开宝箱成功弹窗 -->
     <div class="open-mask" :class="[boxShow ? 'show':'hide']">
       <div class="open-text">+{{prize}}Koin</div>
@@ -248,9 +244,6 @@ export default {
     clearInterval(this.interval);
   },
   methods: {
-    goBack(){
-          window.AndroidWebView.closeActivities()
-    },
     ceshixiaoxi(){
       this.showpopup=false
     },
@@ -717,43 +710,7 @@ export default {
 @import "../../assets/css/task.css";
 </style>
 <style scoped>
-.fixHeader {
-  position: fixed;
-  top: 0;
-  height: 50px;
-  text-align: center;
-  line-height: 50px;
-  font-size: 16px;
-  color: #333333;
-  background:rgba(255,255,255,1);
-  margin-bottom: 8px;
-  width: 100%;
-  z-index: 1;
-  box-shadow:0px 2px 8px 0px rgba(0,0,0,0.06);
-}
 
-.fixHeader div {
-  width: 80%;
-  margin: auto;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  font-weight: bold;
-  height: 50px;
-  line-height: 50px;
-}
-
-.fixHeader .nc-icon-prev {
-    display: inline-block;
-    position:absolute;
-    left: 15px;
-    top: 20px;
-    width: 10px;
-    height: 10px;
-    border-top: 2px solid #000000;
-    border-right: 2px solid #000000;
-    transform: rotate(-135deg);
-}
 .taskBox .double_btn {
   margin: 0 auto;
   width: 85%;
